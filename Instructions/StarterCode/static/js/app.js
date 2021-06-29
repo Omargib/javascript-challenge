445a7a5b76d846d05d007979217cca58aa69e4c6
Omar Gibran Rivera Galvan <omargib86@hotmail.com>
2021-06-29 18:31:43 -0500

    update

--- Instructions/StarterCode/static/js/app.js
+++ Instructions/StarterCode/static/js/app.js
@@ -13,13 +13,25 @@ tableData.forEach(row => {
 }
 function filterData(){
     var date = d3.select("#datetime").property("value")
+    var city = d3.select("#city").property("value")
     var state = d3.select("#state").property("value")
+    var country = d3.select("#country").property("value")
+    var shape = d3.select("#shape").property("value")
     var newData = data
     if (date){
     newData = newData.filter(row => row.datetime === date)    
     }
+    if (city){
+        newData = newData.filter(row => row.city === city)    
+        }
     if (state){
         newData = newData.filter(row => row.state === state)    
+        }
+    if (country){
+        newData = newData.filter(row => row.country === country)    
+        }
+    if (shape){
+        newData = newData.filter(row => row.shape === shape)
         }
     buildTable(newData)
 }
